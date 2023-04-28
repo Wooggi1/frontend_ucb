e1a4d8ec32fcbfe512d0a25b421910938f65bf9d
Thiago Silva <thiago2b9s@gmail.com>
2023-04-27 23:41:43 -0300

    eeeee

---
+++ pizzaria/main.js
@@ -0,0 +1,12 @@
+//nn funciona essa porra nn sei oq fazer
+
+function changeImage1() {
+    var img = document.getElementById("fotoprincipal");
+    img.setAttribute("src", "imgs/pizzaChocolate.jpg");
+  }
+
+  function changeImage2() {
+    var img = document.getElementById("fotoprincipal");
+    img.setAttribute("src", "imgs/Portuguesa.jpg");
+  }
+
